9d16ed7301c724132118593836e1b140b7643145
Konstantin Taletskiy <konstantin@taletskiy.com>
2020-02-11 12:42:09 -0600

    ci: fix missing error handler

--- deploy/tools/railyard/railyard/tools.py
+++ deploy/tools/railyard/railyard/tools.py
@@ -7,7 +7,7 @@ def cd(path):
     os.chdir(path)
     try:
         yield
-    except:
+    except e:
         print(f'Could not change path to: {path}. Error log: {e}')
     finally:
         os.chdir(CWD)
